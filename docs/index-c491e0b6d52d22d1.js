db19867f96e91bf9f9a5cd36738276a9ceb8533b
Braggle <Braggle braggle@bht.nz>
2022-02-09 16:03:26 -0800

    Deploy 5fd22

--- docs/index-c491e0b6d52d22d1.js
+++ docs/index-c491e0b6d52d22d1.js
@@ -302,7 +302,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-46f413e06642c70d_bg.wasm', import.meta.url);
+        input = new URL('index-c491e0b6d52d22d1_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
