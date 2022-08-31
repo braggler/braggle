d4cd1ea29f9ca47e934ed43ad9982c6ed50f8fe7
Braggle <braggle@bht.nz>
2022-08-30 20:43:42 -0700

    Deploy 56929

--- docs/index-804ae9c9c28a47bb.js
+++ docs/index-804ae9c9c28a47bb.js
@@ -302,7 +302,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-f837f85e22a91465_bg.wasm', import.meta.url);
+        input = new URL('index-804ae9c9c28a47bb_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
@@ -672,12 +672,12 @@ async function init(input) {
         var ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1164 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper1259 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 79, __wbg_adapter_24);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper3889 = function(arg0, arg1, arg2) {
-        var ret = makeClosure(arg0, arg1, 227, __wbg_adapter_27);
+    imports.wbg.__wbindgen_closure_wrapper3888 = function(arg0, arg1, arg2) {
+        var ret = makeClosure(arg0, arg1, 231, __wbg_adapter_27);
         return addHeapObject(ret);
     };
 
